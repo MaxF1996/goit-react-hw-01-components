1b1717feb60acd3800aca7dc5362a7e3bb95436a
MaxF1996 <maxeffect.apollo@gmail.com>
2023-03-05 15:50:26 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -13,7 +13,7 @@ const FriendList = ({ friends }) => {
 
 const FriendListItem = ({ avatar, name, isOnline, id }) => {
   return (
-    <li key={id} className={css.item}>
+    <li className={css.item}>
       <span className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
